cd620db10f9a35ad241c8182a1e4c92518aa8ec9
iliakut <ilyakut@gmail.com>
2019-01-02 23:05:52 +0300

    heroku

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,14 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+//added this middleware
+var history = require('connect-history-api-fallback');
+
 app = express()
+//started middleware
+app.use(history());
+
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
