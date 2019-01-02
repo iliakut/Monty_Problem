21b3387feeab7822fad15ccc7eccb1904a726f3e
iliakut <ilyakut@gmail.com>
2019-01-02 15:53:13 +0300

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app = express()
 //started middleware
 app.use(history());
 
-app.use(serveStatic(__dirname))
+app.use(serveStatic(__dirname + "/dist/"))
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
