b3705089a79d2d04f59a435b45bb9683c95950bd
iliakut <ilyakut@gmail.com>
2019-01-02 22:59:10 +0300

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-//added this middleware
-var history = require('connect-history-api-fallback');
-
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 app = express()
-//started middleware
-app.use(history());
-
-app.use(serveStatic(__dirname + "/dist/"))
+app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
